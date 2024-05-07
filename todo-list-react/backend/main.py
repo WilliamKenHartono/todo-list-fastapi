355f1040ac39ec65a58b560a556652b146c5ad4d
WilliamKenHartono <131489082+WilliamKenHartono@users.noreply.github.com>
2024-05-07 22:10:12 +0700

    Adding API

--- todo-list-react/backend/main.py
+++ todo-list-react/backend/main.py
@@ -1,6 +1,26 @@
-from fastapi import FastAPI, HTTPException, Depends
-from typing import Optional, List, Dict
+from fastapi import FastAPI
 from pydantic import BaseModel
-from uuid import UUID, uuid4
 
 app = FastAPI()
+
+tasks = []
+
+class TaskSchema(BaseModel):
+    id: int
+    task_name: str
+    is_completed: bool
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+def get_task_by_id(id: int):
+    for task in tasks:
+        if task.id == id:
+            return task
+
+@app.post("/tasks")
+def create_task(task: TaskSchema):
+    tasks.append(task)
+    return {"success": True, "message": "successfully created task"}
